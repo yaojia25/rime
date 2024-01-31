b34436667f07748e42da686b5a59a65a780fa9f8
Jasyo <31342892+yaojia25@users.noreply.github.com>
2024-01-31 17:13:21 +0800

    feat: add kkgithub proxy

--- src/rime_deploy/metadata.py
+++ src/rime_deploy/metadata.py
@@ -7,6 +7,7 @@ import enum
 class Proxy(enum.Enum):
     GHPROXY = "https://ghproxy.net/"
     MOEYY = "https://github.moeyy.xyz/"
+    KKGITHUB = "https://github.com/"
 
 
 class RimeSchema(enum.Enum):
@@ -14,3 +15,11 @@ class RimeSchema(enum.Enum):
     """雾凇拼音"""
     RIME_CLOVER_PINYIN = "https://github.com/fkxxyz/rime-cloverpinyin.git"
     """四叶草拼音"""
+
+def splice_proxy(proxy: Proxy, url: str) -> str:
+    if proxy in [Proxy.GHPROXY, Proxy.MOEYY]:
+        return proxy.value + url
+    elif proxy in [Proxy.KKGITHUB]:
+        return url.replace("https://github.com/", proxy.value)
+    else:
+        return url
